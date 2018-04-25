c67179e8c04b03db7ee5a971ce08580bcb43899f
Your Name <you@example.com>
2018-04-25 13:58:06 -0400

    The app is fully functional!

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,39 @@
   <h1>Word Translator</h1>
   <h5>Powered by Vue.js</h5>
   <translateForm v-on:formSubmit="translateText"></translateForm>
+  <translateOutput v-text="translatedText"></translateOutput>
 </div>
 </template>
 
 <script>
 import translateForm from './components/translateForm'
+import translateOutput from './components/translateOutput'
 
 export default {
   name: 'App',
   components: {
-    translateForm
+    translateForm,
+    translateOutput
+  },
+  data: function() {
+    return {
+      translatedText: ''
+    }
   },
   methods: {
-    translateText: function(text) {}
+    translateText: function(text, language) {
+      this.$http
+        .get(
+          'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180422T204748Z.eb9b55d86498f560.cdc9e4f343a84245fed10909abba494e50524223&lang=' +
+            language +
+            '&text=' +
+            text
+        )
+
+        .then(response => {
+          this.translatedText = response.body.text[0]
+        })
+    }
   }
 }
 </script>
--- src/components/translateForm.vue
+++ src/components/translateForm.vue
@@ -2,6 +2,14 @@
   <div id="translateForm">
     <form v-on:submit="formSubmit">
         <input type= 'text' v-model= "textToTranslate" placeholder= "Enter a word...">
+        <select v-model= "language">
+          <option value="es">Spanish</option>
+          <option value="ru">Russian</option>
+          <option value="fr">Frence</option>
+          <option value="zh">Chinese</option>
+          <option value="de">German</option>
+          <option value="sv">Swedish</option>
+        </select>
         <input type= "submit" value = "Translate">
     </form>
 </div>
@@ -12,12 +20,16 @@ export default {
   name: 'translateForm',
   data() {
     return {
-      textToTranslate: ''
+      textToTranslate: '',
+      language: ''
     }
+  },
+  created() {
+    this.language = 'es'
   },
   methods: {
     formSubmit(e) {
-      this.$emit('formSubmit', this.textToTranslate)
+      this.$emit('formSubmit', this.textToTranslate, this.language)
       e.preventDefault()
     }
   }
