c28cdacb902aa5788f2f6e9953e201316115ec0b
Your Name <you@example.com>
2018-04-25 00:34:36 -0400

    1-text-box-and-submit-button

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+  <h1>Word Translator</h1>
+  <h5>Powered by Vue.js</h5>
+  <translateForm v-on:formSubmit="translateText"></translateForm>
+</div>
+</template>
+
+<script>
+import translateForm from './components/translateForm'
+
+export default {
+  name: 'App',
+  components: {
+    translateForm
+  },
+  methods: {
+    translateText: function(text) {}
+  }
+}
+</script>
+
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/translateForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="translateForm">
+    <form v-on:submit="formSubmit">
+        <input type= 'text' v-model= "textToTranslate" placeholder= "Enter a word...">
+        <input type= "submit" value = "Translate">
+    </form>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'translateForm',
+  data() {
+    return {
+      textToTranslate: ''
+    }
+  },
+  methods: {
+    formSubmit(e) {
+      this.$emit('formSubmit', this.textToTranslate)
+      e.preventDefault()
+    }
+  }
+}
+</script>
+
+
+
+<style>
+</style>
